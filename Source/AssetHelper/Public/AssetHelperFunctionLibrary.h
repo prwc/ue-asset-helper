c99c859d74146c13bcc5c60621f1702b887dbb23
Peerawit Canthasaksiri <5309753+prwc@users.noreply.github.com>
2023-07-12 14:36:34 +0700

    added CRC helper for Texture2D type

---
+++ Source/AssetHelper/Public/AssetHelperFunctionLibrary.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AssetHelperFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSETHELPER_API UAssetHelperFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	UFUNCTION(BlueprintCallable)
+	static int32 CalculateTextureCRC(UTexture2D* Texture2D)
+	{
+		uint32 Result;
+		return Texture2D->GetSourceArtCRC(Result) ? Result : -1;
+	}
+};
